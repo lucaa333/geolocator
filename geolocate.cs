cef05c2903ddf182821d665efb04d78465047604
Luca <luca.sorg911@gmail.com>
2024-10-10 22:13:43 +0200

    add try catch

--- geolocate.cs
+++ geolocate.cs
@@ -10,6 +10,28 @@ using Newtonsoft.Json;
 namespace geolocate
 {
 
+
+    public class Data 
+    {
+
+        public string city { get; set; }
+
+        public string region { get; set; }
+        
+        public string country { get; set; }
+
+        public string loc { get; set; }
+
+        public string org { get; set; }
+
+        public string postal { get; set; }
+
+        public string timezone { get; set; }
+
+
+
+    }
+
     internal class Program
     {
 
@@ -20,6 +42,32 @@ namespace geolocate
             string ip = Console.ReadLine();
             string url = $"https://ipinfo.io/{ip}json";
 
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+
+                    Console.WriteLine("[+] Request Successfully Made");
+
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    Data ipInfo = JsonConvert.DeserializeObject<Data>(responseData);
+
+                    Console.Clear();
+                    Console.WriteLine($"Country: {ipInfo.country}");
+                    Console.WriteLine($"City: {ipInfo.city}");
+                    Console.WriteLine($"Coordinates: {ipInfo.loc}");
+                    Console.WriteLine($"Postal Code: {ipInfo.postal}");
+                    Console.WriteLine($"Region: {ipInfo.region}");
+                    Console.WriteLine($"ASN: {ipInfo.org}");
+
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
         }
     }
 }
